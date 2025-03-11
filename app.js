54a293ffcf971f3ee12535c48b2843e4fc00a587
Joy Dutta <dutta_joy@hotmail.com>
2025-03-11 09:14:12 +0000

    renames the file to controller/api.controllers.js, removes the middleware to parse the body at this stage

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require("express");
-const { getApi } = require("./controllers/api.controller");
+const { getApi } = require("./controllers/api.controllers");
 
 const app = express();
-
-app.use(express.json());
 
 app.get("/api", getApi);
 
