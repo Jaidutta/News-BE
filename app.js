e9c4cfe42ba30e987a85bbb2ff8b2461d6519a55
Joy Dutta <dutta_joy@hotmail.com>
2025-03-31 14:54:31 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cors = require('cors');
+
 const { getApi } = require("./controllers/api.controllers");
 const { 
   getAllArticles, 
@@ -26,6 +28,8 @@ const {
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.get("/api", getApi);
 
